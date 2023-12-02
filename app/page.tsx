56a9d9640deaa700ae7fe5a8f166d6993fdeba40
Mario <marioalexander998@gmail.com>
2023-12-02 21:22:58 +0700

    feat: changing theme functionality

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 // Node Modules
 import { useState } from "react";
+import cn from "classnames";
 
 // Component
 import Toggle from "@/components/Toggle";
@@ -14,14 +15,17 @@ const CalculatorPage = () => {
   };
 
   return (
-    <main className="min-h-screen bg-main-1">
-      <div className="block max-w-xl mx-auto py-20">
+    <main className={cn("min-h-screen", `bg-main-${toggleTheme}`)}>
+      <div className="block max-w-xl mx-auto py-20 px-6">
         <div className="flex justify-between space-x-0 h-full w-full items-center">
-          <h1 className="text-2xl font-bold text-white ">calc</h1>
+          <h1 className={cn("text-2xl font-bold", `font-color-${toggleTheme}`)}>calc</h1>
           <div className="flex">
-            <p className="text-sm font-bold text-white mr-8 pb-1 self-end">THEME</p>
+            <p className={cn("text-sm font-bold mr-8 pb-1 self-end", `font-color-${toggleTheme}`)}>THEME</p>
             <Toggle currentTogglePosition={toggleTheme} onClickToggle={handleClickToggle} toggleAmount={3} />
           </div>
+        </div>
+        <div className={cn("h-32 w-full mt-6 rounded-lg", `bg-content-var-a-${toggleTheme}`)}>
+
         </div>
       </div>
     </main>
